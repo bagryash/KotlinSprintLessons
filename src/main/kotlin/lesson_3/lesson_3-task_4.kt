f7dd042364bd2446b9ca80fd51a853d722ebeec0
alexeybagrashov <alexey.bagryashov@yandex.ru>
2025-02-17 21:52:50 +0300

    KLS-3-4 add moves of white pieces

---
+++ src/main/kotlin/lesson_3/lesson_3-task_4.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_3
+
+fun main() {
+
+    var moveNumber = 1
+    var startingPosition = "E2"
+    var finalPosition = "E4"
+
+    var playersMove = println("$startingPosition - $finalPosition $moveNumber")
+
+    moveNumber += 2
+    startingPosition = "D2"
+    finalPosition = "D3"
+    playersMove = println("$startingPosition - $finalPosition $moveNumber")
+
+}
