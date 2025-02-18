b643684367657fcc674b9749dc91376b9c4e5d8f
alexeybagrashov <alexey.bagryashov@yandex.ru>
2025-02-18 19:48:41 +0300

    KLS-4-1 add tables available today and tomorrow

---
+++ src/main/kotlin/lesson_4/lesson_4-task_1.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_4
+
+fun main() {
+
+    val  numberFreeTablesToday = 13
+    val numberFreeTablesTomorrow = 9
+
+    val tablesAvailableToday = !(NUMBER_OF_TABLES == numberFreeTablesToday)
+    val tablesAvailableTomorrow = !(NUMBER_OF_TABLES == numberFreeTablesTomorrow)
+
+    println("[Доступность столиков на сегодня: $tablesAvailableToday],\n[Доступность столиков на завтра: $tablesAvailableTomorrow].")
+
+}
+const val NUMBER_OF_TABLES = 13
