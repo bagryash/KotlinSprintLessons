be818d6f576f337290daa76f987ab0e06c389225
alexeybagrashov <alexey.bagryashov@yandex.ru>
2025-02-20 14:40:48 +0300

    KS-3-1 add welcome screen

--- src/main/kotlin/lesson_3/lesson_3-task_1.kt
+++ src/main/kotlin/lesson_3/lesson_3-task_1.kt
@@ -2,21 +2,11 @@ package org.example.lesson_3
 
 fun main() {
 
-//    val userName = "Иван"
-//    val goodText = "Добрый"
-//    val dayText = "день"
-//    val nightText = "вечер"
-//
-//    var greetingsText = println("$goodText $dayText, $userName!")
-//    greetingsText = println("$goodText $nightText, $userName!")
-
     val userName = "Иван"
     var timesOfDayText = "день"
-    var goodText = "Добрый $timesOfDayText, $userName"
-    var greetingsText = println(goodText)
+    var greetingsText = println("Добрый $timesOfDayText, $userName")
 
     timesOfDayText = "вечер"
-    goodText = "Добрый $timesOfDayText, $userName"
-    greetingsText = println(goodText)
+    greetingsText = println("Добрый $timesOfDayText, $userName")
 
 }
