e7b4083391291380b2a2ed88be435529ed732cf2
alexeybagrashov <alexey.bagryashov@yandex.ru>
2025-02-20 14:54:30 +0300

    KS-3-2 add surname, name, patronymic, age

--- src/main/kotlin/lesson_3/lesson_3-task_2.kt
+++ src/main/kotlin/lesson_3/lesson_3-task_2.kt
@@ -2,6 +2,16 @@ package org.example.lesson_3
 
 fun main() {
 
+    val userAge = 20
+    val userSurname = "Андреева"
+    val userName = "Татьяна"
+    val userPatronymic = "Сергеевна"
+
+    val userAgeNew = 22
+    val userSurnameNew = "Сидорова"
+
+    println("$userSurname $userName $userPatronymic, $userAge")
+    println("$userSurnameNew $userName $userPatronymic, $userAgeNew")
 
 
 }
