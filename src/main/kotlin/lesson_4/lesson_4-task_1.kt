a26a8f5035356249d8dab9bbd2b11c8482eae907
alexeybagrashov <alexey.bagryashov@yandex.ru>
2025-02-21 21:26:29 +0300

    KS-4-1 add corrections based on comments

--- src/main/kotlin/lesson_4/lesson_4-task_1.kt
+++ src/main/kotlin/lesson_4/lesson_4-task_1.kt
@@ -5,10 +5,10 @@ fun main() {
     val numberFreeTablesToday = 13
     val numberFreeTablesTomorrow = 9
 
-    val tablesAvailableToday = !(NUMBER_OF_TABLES == numberFreeTablesToday)
-    val tablesAvailableTomorrow = !(NUMBER_OF_TABLES == numberFreeTablesTomorrow)
+    val isTablesAvailableToday = !(NUMBER_OF_TABLES == numberFreeTablesToday)
+    val isTablesAvailableTomorrow = !(NUMBER_OF_TABLES == numberFreeTablesTomorrow)
 
-    println("[Доступность столиков на сегодня: $tablesAvailableToday],\n[Доступность столиков на завтра: $tablesAvailableTomorrow].")
+    println("[Доступность столиков на сегодня: $isTablesAvailableToday],\n[Доступность столиков на завтра: $isTablesAvailableTomorrow].")
 
 }
 const val NUMBER_OF_TABLES = 13
