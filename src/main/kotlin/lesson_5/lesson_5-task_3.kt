1abb8f00581a7e5cf5ebfad4ebb2a3e0ddcc48c9
alexeybagrashov <alexey.bagryashov@yandex.ru>
2025-02-23 14:39:21 +0300

    KS-5-3 add  fix by comment

--- src/main/kotlin/lesson_5/lesson_5-task_3.kt
+++ src/main/kotlin/lesson_5/lesson_5-task_3.kt
@@ -10,16 +10,6 @@ fun main() {
     println("Введите второе число от 0 до 42:")
     val userNumberTwo = readln().toInt()
 
-    if (userNumberOne == numberOne)
-        if (userNumberTwo == numberTwo) println("Поздравляем! Вы выиграли главный приз!")
-        else println("Вы выиграли утешительный приз!")
-    else if (userNumberOne == numberTwo)
-        if (userNumberTwo == numberOne) println("Поздравляем! Вы выиграли главный приз!")
-        else println("Вы выиграли утешительный приз!")
-    else if (userNumberTwo == numberOne) println("Вы выиграли утешительный приз!")
-    else if (userNumberTwo == numberTwo) println("Вы выиграли утешительный приз!")
-    else println("Неудача!")
-
     when (userNumberOne) {
         numberOne -> when (userNumberTwo) {
             numberTwo -> println("Поздравляем! Вы выиграли главный приз!")
