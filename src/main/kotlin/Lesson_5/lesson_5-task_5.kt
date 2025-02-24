74a642dfd115c047001c20a28a29f02d23654f29
alexeybagrashov <alexey.bagryashov@yandex.ru>
2025-02-24 18:50:34 +0300

    KS-5-5 new lottery 3 from 0 to 42

--- src/main/kotlin/Lesson_5/lesson_5-task_5.kt
+++ src/main/kotlin/Lesson_5/lesson_5-task_5.kt
@@ -1,68 +1,22 @@
 package org.example.lesson_5
 
 fun main() {
+    val numbers = mutableListOf((0..42).random(), (0..42).random(), (0..42).random())
 
-
-    val numbers = mutableListOf((0..42).random(), (0..42).random(),(1..10).random()  )
     println("Введите три числа от 0 до 42")
+    val userNumber = mutableListOf(readln(), readln(), readln())
 
-val userNumber = mutableListOf(readln(), readln(), readln())
+    val intersection = numbers.intersect(userNumber)
 
-    println("Введите первое число от 0 до 42:")
-    val userNumberOne = readln().toInt()
-    println("Введите второе число от 0 до 42:")
-    val userNumberTwo = readln().toInt()
-    println("Введите третье число от 0 до 42:")
-    val userNumberThree = readln().toInt()
-
-    when (userNumberOne) {
-        numbers[0] ->
-            if ((userNumberTwo == numbers[1]) and (userNumberThree == numbers[2])) {
-                println(THREE)
-            }  else if ((userNumberTwo == numbers[2]) and (userNumberThree == numbers[1])){
-                println(THREE)
-            } else if  ((userNumberTwo == numbers[1]) ||  (userNumberThree == numbers[2])){
-                println(TWO)
-            }  else if  ((userNumberTwo == numbers[2]) ||  (userNumberThree == numbers[1])) {
-                println(TWO)
-            } else println("Вы не угадали ни одного числа!")
-
-        numbers[1] ->
-            if ((userNumberTwo == numbers[0]) and (userNumberThree == numbers[2])) {
-                println(THREE)
-            }  else if ((userNumberTwo == numbers[2]) and (userNumberThree == numbers[0])){
-                println(THREE)
-            } else if  ((userNumberTwo == numbers[0]) ||  (userNumberThree == numbers[2])){
-                println(TWO)
-            }  else if  ((userNumberTwo == numbers[2]) ||  (userNumberThree == numbers[0])) {
-                println(TWO)
-            } else println("Вы не угадали ни одного числа!")
-
-
-
-        numbers[2] ->
-            if ((userNumberTwo == numbers[0]) and (userNumberThree == numbers[1])) {
-                println(THREE)
-            }  else if ((userNumberTwo == numbers[1]) and (userNumberThree == numbers[0])){
-                println(THREE)
-            } else if  ((userNumberTwo == numbers[0]) ||  (userNumberThree == numbers[1])){
-                println(TWO)
-            }  else if  ((userNumberTwo == numbers[1]) ||  (userNumberThree == numbers[0])) {
-                println(TWO)
-            } else println("Вы не угадали ни одного числа!")
-
-        else ->
-            if (userNumberTwo == numberOne) {
-                println("Вы выиграли утешительный приз!")
-            } else if (userNumberTwo == numberTwo) {
-                println("Вы выиграли утешительный приз!")
-            } else {
-                println("Неудача!")
-            }
+    when (intersection.size) {
+        0 -> println(ZERO_COINCIDENCE)
+        1 -> println(ONE_COINCIDENCE)
+        2 -> println(TWO_COINCIDENCE)
+        3 -> println(THREE_COINCIDENCE)
     }
 }
 
-const val THREE = "Поздравляем! Вы выиграли джекпот!!!"
-const val TWO =  "Вы выиграли крупный приз!"
-const val ONE =  "Вы выиграли утешительный приз!"
-const val ZERO =  "Вы не угадали ни одного числа!"
+const val ZERO_COINCIDENCE = "Вы не угадали ни одного числа!"
+const val ONE_COINCIDENCE = "Вы выиграли утешительный приз!"
+const val TWO_COINCIDENCE = "Вы выиграли крупный приз!"
+const val THREE_COINCIDENCE = "Поздравляем! Вы выиграли джекпот!!!"
