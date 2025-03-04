9c02e1fc010db2fea5e08b2083d9cce1ec450196
alexeybagrashov <alexey.bagryashov@yandex.ru>
2025-03-04 21:14:18 +0300

    KS-6-5 add new capcha

---
+++ src/main/kotlin/lesson_6/lesson_6-task_5.kt
@@ -0,0 +1,30 @@
+package org.example.lesson_6
+
+fun main() {
+    val sumForCheck = (1..10).random()
+    println("Введите для проверки 2 простых числа, сумма которых равно $sumForCheck")
+    var counter = 3
+
+    println("Первое число:")
+    var numberOne = readln().toInt()
+    println("Второе число:")
+    var numberTwo = readln().toInt()
+    counter--
+
+    while (numberOne + numberTwo != sumForCheck && counter > ZERO) {
+        println("У вас еще попыток: $counter")
+        println("Первое число:")
+        numberOne = readln().toInt()
+        println("Второе число:")
+        numberTwo = readln().toInt()
+        counter--
+    }
+
+    if (numberOne + numberTwo == sumForCheck) {
+        println("Добро пожаловать!")
+    } else {
+        println("Доступ запрещен.")
+    }
+}
+
+const val ZERO = 0
