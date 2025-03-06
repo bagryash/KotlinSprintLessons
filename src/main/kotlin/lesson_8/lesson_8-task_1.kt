6cf0c4d6c03ce22a3f9bb9472b8f1200eee2057c
alexeybagrashov <alexey.bagryashov@yandex.ru>
2025-03-06 17:41:59 +0300

    KS-8-1 add number of ad views per day

---
+++ src/main/kotlin/lesson_8/lesson_8-task_1.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_8
+
+fun main() {
+    val numberOfViewsDays = arrayOf(13, 23, 34, 45, 23, 45, 46)
+    var numberOfViewsWeek = 0
+
+    for (i in numberOfViewsDays) {
+        numberOfViewsWeek = numberOfViewsWeek + i
+    }
+    println(numberOfViewsWeek)
+}
