67d91449fb495073f8997cde7da597b04299f44c
alexeybagrashov <alexey.bagryashov@yandex.ru>
2025-03-06 20:23:56 +0300

    KS-8-3 add ingredient search without loop

--- src/main/kotlin/lesson_8/lesson_8-task_3.kt
+++ src/main/kotlin/lesson_8/lesson_8-task_3.kt
@@ -1,7 +1,34 @@
 package org.example.lesson_8
 
+
 fun main() {
+    var arrayOfIngredients =
+        arrayOf(
+            "Свинина",
+            "Говядина",
+            "Морковь",
+            "Капуста",
+            "Лук репчатый",
+            "Свекла",
+            "Картошка",
+            "Перец горошком",
+            "Лавровый лист",
+            "Петрушка",
+            "Укроп",
+            "Соль",
+            "Вода",
+        )
 
+    println("Введите ингредиент, который хотите найти:")
+    val desiredIngredient = readln()
+    val y = arrayOfIngredients.indexOf(desiredIngredient)
+    println(y)
+    val x = desiredIngredient in arrayOfIngredients
+    println(x)
 
+    if (x == true) println()
+    else println()
 
 }
+
+
