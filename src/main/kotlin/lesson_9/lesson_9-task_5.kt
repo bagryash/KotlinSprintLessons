7d1b6a816580d1b7215ad7618db27c18a1ae0160
alexeybagrashov <alexey.bagryashov@yandex.ru>
2025-03-07 23:57:16 +0300

    KS-9-5 add sorted user ingredients entered individually

--- src/main/kotlin/lesson_9/lesson_9-task_5.kt
+++ src/main/kotlin/lesson_9/lesson_9-task_5.kt
@@ -1,8 +1,21 @@
 package org.example.lesson_9
 
+import java.util.*
+
 fun main() {
+    val range = 1..5
+    println("Введите ингредиенты по одному:")
+    val usersIngredientsSet = mutableSetOf<String>()
+    for (i in range) {
+        val ingredient = readln()
+        usersIngredientsSet.add(ingredient)
+    }
 
-    var orderCount : Int = 75
-    var thanksForOrderText : String = "Спасибо, что выбрали наш магазин!"
+    var usersIngredientsList = usersIngredientsSet.toMutableList()
+    usersIngredientsList.sort()
+    val firstIngredient =
+        usersIngredientsList[0].replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
+    usersIngredientsList[0] = firstIngredient
 
+    println(usersIngredientsList)
 }
