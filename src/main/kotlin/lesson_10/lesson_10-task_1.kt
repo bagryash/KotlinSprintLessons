e21536582299526fed770094c35bb16177d91dbb
alexeybagrashov <alexey.bagryashov@yandex.ru>
2025-03-12 13:34:07 +0300

    KS-10-1 add dice

--- src/main/kotlin/lesson_10/lesson_10-task_1.kt
+++ src/main/kotlin/lesson_10/lesson_10-task_1.kt
@@ -3,27 +3,28 @@ package org.example.lesson_10
 import java.lang.Thread.sleep
 
 fun main() {
-    println("Бросает человек")
-    sleep(1000)
-    val x = throwDice()
+    println("Бросает человек:")
     sleep(THOUSAND)
-    println("Бросает машина")
+    val humanDiceSum = throwDice() + throwDice()
     sleep(THOUSAND)
-    val y = throwDice()
+    println("Бросает машина:")
+    val robotDiceSum = throwDice() + throwDice()
     sleep(THOUSAND)
-    if (x > y) println("Победило человечество!")
-    else if (y > x) println("Победила машина!")
-    else println("Победила дружба!")
+
+    if (humanDiceSum > robotDiceSum) {
+        println("Победило человечество!")
+    } else if (humanDiceSum < robotDiceSum) {
+        println("Победила машина!")
+    } else {
+        println("Победила дружба!")
+    }
 }
 
 fun throwDice(): Int {
     val cubeRange = 1..6
     val meaningOfFirstCube = (cubeRange).random()
     println(meaningOfFirstCube)
-    val meaningOfSecondCube = (cubeRange).random()
-    println(meaningOfSecondCube)
-    val sum = meaningOfFirstCube + meaningOfSecondCube
-    return sum
+    return meaningOfFirstCube
 }
 
 const val THOUSAND = 1000L
