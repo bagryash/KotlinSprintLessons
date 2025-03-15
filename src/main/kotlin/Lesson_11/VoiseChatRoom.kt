2099a1b8b7208abafb23d0844074bab73a645065
admin <12345678>
2025-03-15 13:54:42 +0300

    KS-11-3 add vois chat

---
+++ src/main/kotlin/Lesson_11/VoiseChatRoom.kt
@@ -0,0 +1,32 @@
+package org.example.Lesson_11
+
+import javax.swing.ImageIcon
+
+class VoiseChatRoom(
+    roomIcon: ImageIcon,
+    roomName: String,
+    users: Map<String, Boolean>?
+
+) {
+
+    fun addUser(){
+        println()
+    }
+
+    fun changeUserStatus(){
+
+    }
+
+}
+
+
+class User(
+    userIcon: ImageIcon,
+    userName: String,
+    userStatus: Boolean?
+
+) {
+
+
+
+}
---
+++ src/main/kotlin/Lesson_11/lesson_11-task_3.kt
@@ -0,0 +1,6 @@
+package org.example.Lesson_11
+
+fun main() {
+
+
+}
