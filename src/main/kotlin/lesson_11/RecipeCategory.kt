228701a451c0eea7076c20058e34120c6048c598
admin <12345678>
2025-03-15 18:48:35 +0300

    KS-11-4 add classes for recipe app

--- src/main/kotlin/lesson_11/RecipeCategory.kt
+++ src/main/kotlin/lesson_11/RecipeCategory.kt
@@ -1,23 +1,27 @@
 package org.example.lesson_11
 
+import java.awt.Image
+
 class RecipeCategory(
-    val id:Int,
-    val category: String,
-) {
-}
+    val id: Int,
+    val image: Image,
+    val categoryName: String,
+) {}
 
 class Recipe(
-    val id:Int,
-    val ingredients: List<String>,
-    val category : String,
-) {
-}
+    val id: Int,
+    val image: Image,
+    val category: String,
+    val ingredientsList: List<String>,
+    val methodOfPreparation: List<String>,
+    var isInFavorites: Boolean = false
+) {}
 
 
 class Ingredient(
-    val id:Int,
-    val ingredientName : String,
-    val quantity : Int,
-) {
-}
+    val id: Int,
+    var name: String,
+    val volume: Int,
+    val
+) {}
 
