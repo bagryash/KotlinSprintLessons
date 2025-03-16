d2f7314dfe42660e008cebff7fac90a0e6dc46c9
admin <12345678>
2025-03-16 17:19:14 +0300

    KS-11-3 add classes for voice chat

--- src/main/kotlin/lesson_11/lesson_11-task_5.kt
+++ src/main/kotlin/lesson_11/lesson_11-task_5.kt
@@ -8,6 +8,34 @@ fun main() {
     val m1 = ForumMessage(1,"Привет, Шелдон!!!")
     val m2 = ForumMessage(2, "Вуфф-вуфф!")
 
+}
 
 
+class Forum() {
+    fun createNewUser(id: Int, name: String){
+        val userId = ForumMember.id
+    }
+
+    fun createNewMessage(id:Int){
+
+    }
+
+    fun printThread(){
+
+    }
+}
+
+class ForumBuilder(){
+
+}
+
+class ForumMember(
+    var userId: Int,
+    var userName: String
+)
+
+class ForumMessage(
+    val authorId: Int,
+    val message: String,
+) {
 }
