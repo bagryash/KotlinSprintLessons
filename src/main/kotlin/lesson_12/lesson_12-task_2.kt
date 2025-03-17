72e811504ae3dcbeca2c30cf445f330748c6a6e8
admin <12345678>
2025-03-17 20:31:25 +0300

    KS-12-2 add new program tracks weather by day

---
+++ src/main/kotlin/lesson_12/lesson_12-task_2.kt
@@ -0,0 +1,22 @@
+package org.example.lesson_12
+
+class WeatherDayAndNight(
+    var daytimeTemperature: Int,
+    var temperatureAtNight: Int,
+    var isPrecipitation: Boolean,
+) {
+
+
+    fun printlnWeatherDayAndNight() = println(
+        """
+        |Температура днем: $daytimeTemperature,
+        |Температура ночью: $temperatureAtNight,
+        |Осадки сегодня: $isPrecipitation
+    """.trimMargin()
+    )
+}
+
+fun main() {
+    val weather15012025 = WeatherDayAndNight(0, -4, true)
+    weather15012025.printlnWeatherDayAndNight()
+}
