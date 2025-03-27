47d9fb8866ec563bade02a2fe717f61517e133c3
alexeybagrashov <alexey.bagryashov@yandex.ru>
2025-03-27 16:37:21 +0300

    KS-13-2 add phone directory with output to console

--- src/main/kotlin/lesson_13/lesson_13-task_2.kt
+++ src/main/kotlin/lesson_13/lesson_13-task_2.kt
@@ -1,7 +1,19 @@
 package org.example.lesson_13
 
+class PhoneDirectoryWithPrint(
+    val name: String,
+    val phoneNumber: Long,
+    val company: String? = null
+) {
+    val printPhoneDirectory =
+        println(
+                "- Имя: $name\n" +
+                "- Номер: $phoneNumber\n" +
+                "- Компания: ${company ?: "не указано"}",
+        )
+}
+
 fun main() {
-
-
-
+    val inessaPhone = PhoneDirectoryWithPrint("Инесса", 88002500890)
+    inessaPhone.printPhoneDirectory
 }
