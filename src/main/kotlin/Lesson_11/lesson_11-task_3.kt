6bbb348180c89e8917649ca51c09d96b28fee983
alexeybagrashov <alexey.bagryashov@yandex.ru>
2025-03-27 13:40:11 +0300

    KS-11-3 add fix by comment

--- src/main/kotlin/Lesson_11/lesson_11-task_3.kt
+++ src/main/kotlin/Lesson_11/lesson_11-task_3.kt
@@ -1,12 +1,12 @@
 package org.example.Lesson_11
 
 fun main() {
-    val room1 = VoiceChatRoom(1, 1, "Рыбалка", listOf(), listOf(), listOf(), listOf())
+    val room1 = VoiceChatRoom(1, 1, "Рыбалка", mutableListOf())
+    val alex = User ( 1, 999, "Alex", "alex@mail.com", "5z9q5a", "on" )
 
     println("Какой пользователь вошел в комнату?:")
-    val newUserInRoomId = readln().toInt()
-    val newUserInRoomName = readln()
-    room1.addUser(newUserInRoomId, newUserInRoomName )
+    val newUserInRoomName: User = alex
+    room1.addUser(newUserInRoomName)
 
     println("Какой пользователь включил микрофон(id)?:")
     val newUserWithMikeOn = readln().toInt()
@@ -17,32 +17,25 @@ class VoiceChatRoom(
     id: Int,
     var roomIcon: Int,
     var roomName: String,
-    private var usersInRoom: List<String>,
-    private var usersMikeOn: List<Int>,
-    private var usersMikeMute: List<Int>,
-    private var usersWithMikeDisable: List<Int>
+    var users: MutableList<User>,
 ) {
-
-    fun addUser(id: Int, user: String) {
-        usersMikeMute += id
-        usersInRoom += user
+    fun addUser(
+        user: User,
+    ) {
+        users += user
     }
 
     fun userMikeOn(id: Int) {
         val newUserStatusId = readln().toInt()
-        usersMikeOn += newUserStatusId
-        usersMikeMute -= newUserStatusId
-        usersWithMikeDisable -= newUserStatusId
     }
 }
 
-public class User(
+class User(
     var id: Int,
     var userIcon: Int,
     var userName: String,
     var login: String,
     var password: String,
     var userStatus: String,
-    var mikeStatus: String = "mute"
-) {
-}
+    var mikeStatus: String = "mute",
+)
