ad6746de989317550dd42ec731c14a39f270903b
alexeybagrashov <alexey.bagryashov@yandex.ru>
2025-03-29 22:36:39 +0300

    KS-13-3 add contacts list

---
+++ src/main/kotlin/lesson_13/lesson_13-task_3.kt
@@ -0,0 +1,26 @@
+package org.example.lesson_13
+
+class TelephoneDirectoryWithAllRecords(
+    val name: String,
+    val phoneNumber: Long,
+    val company: String?,
+) {
+    fun printInfo() {
+        println("$name, $phoneNumber, $company")
+    }
+}
+
+fun main() {
+    val contactsList = mutableListOf<TelephoneDirectoryWithAllRecords>()
+
+    val inessaPhone = TelephoneDirectoryWithAllRecords("Инесса", 88002500890, null)
+    contactsList += inessaPhone
+    val tigranPhone = TelephoneDirectoryWithAllRecords("Тигран", 88002509639, "Яндекс")
+    contactsList += tigranPhone
+    val artemyPhone = TelephoneDirectoryWithAllRecords("Артемий", 84959261800, "Студия Лебедева")
+    contactsList += artemyPhone
+    val evgeniyPhone = TelephoneDirectoryWithAllRecords("Артемий", 88007008811, null)
+    contactsList += evgeniyPhone
+    val dmitryPhone = TelephoneDirectoryWithAllRecords("Артемий", 848001006427, "null")
+    contactsList += dmitryPhone
+}
