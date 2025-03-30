66f48f68b380483a6bd4a09c428a14f486188323
alexeybagrashov <alexey.bagryashov@yandex.ru>
2025-03-30 16:59:03 +0300

    KS-11-5 fix by comments

--- src/main/kotlin/lesson_11/lesson_11-task_5.kt
+++ src/main/kotlin/lesson_11/lesson_11-task_5.kt
@@ -1,41 +1,65 @@
 class Forum(
     var users: MutableList<ForumMember> = mutableListOf(),
-    var messages: MutableList<String> = mutableListOf(),
+    var messages: MutableList<ForumMessage> = mutableListOf(),
 ) {
-    fun printThread() {
-        messages.forEach {
-            println(it)
-        }
-    }
-
-    class ForumMember(
-        val userId: Int,
-        val userName: String,
-    )
-
     var idCounter = 0
 
     fun createNewUser(userName: String): ForumMember {
         val newUser = ForumMember(++idCounter, userName)
-        users.add(userName)
+        users.add(newUser)
         return newUser
     }
 
-    fun createNewMessage(id: Int){
-        for (i in Forum.ForumMember)
+    fun createNewMessage(id: Int) {
+        users.forEach {
+            if (id == it.userId) {
+                println("Пользователь вводит сообщение:")
+                val message = readln()
+                val messageFromUser = ForumMessage(it.userName, message)
+                messages.add(messageFromUser)
+            }
+        }
+    }
+
+    fun printThread() {
+        messages.forEach {
+            println(it.userName + ": " + it.useMessage)
+        }
     }
 }
+
+class ForumMember(
+    val userId: Int,
+    val userName: String,
+)
+
+class ForumMessage(
+    val userName: String,
+    val useMessage: String,
+)
 
 fun main() {
     val chat = Forum()
 
     println("Введите имя нового пользователя:")
     val nameUser1 = readln()
-    chat.createNewUser(nameUser1)
+    val user1 = chat.createNewUser(nameUser1)
 
     println("Введите имя нового пользователя:")
     val nameUser2 = readln()
-    chat.createNewUser(nameUser2)
+    val user2 = chat.createNewUser(nameUser2)
 
+    println("Первое сообщение от первого пользователя:")
+    val message1User1 = chat.createNewMessage(user1.userId)
 
+    println("Первое сообщение от второго пользователя:")
+    val message1User2 = chat.createNewMessage(user2.userId)
+
+    println("Первое сообщение от первого пользователя:")
+    val message2User1 = chat.createNewMessage(user1.userId)
+
+    println("Первое сообщение от второго пользователя:")
+    val message2User2 = chat.createNewMessage(user2.userId)
+
+    chat.printThread()
 }
