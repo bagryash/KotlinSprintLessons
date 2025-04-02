0777c1889d687892426cd6506ebf9906d05d0e3c
alexeybagrashov <alexey.bagryashov@yandex.ru>
2025-04-02 15:00:39 +0300

    KS-13-3 add fix by comments

--- src/main/kotlin/lesson_13/lesson_13-task_3.kt
+++ src/main/kotlin/lesson_13/lesson_13-task_3.kt
@@ -3,21 +3,15 @@ package org.example.lesson_13
 class TelephoneDirectoryWithAllRecords(
     val name: String,
     val phoneNumber: Long,
-    val company: String? = null
+    val company: String? = null,
 ) {
     fun printInfo() =
         println(
             "- Имя: $name\n" +
-                    "- Номер: $phoneNumber\n" +
-                    "- Компания: ${company ?: "не указано"}",
+                "- Номер: $phoneNumber\n" +
+                "- Компания: ${company ?: "не указано"}",
         )
 }
-
-fun main() {
-    val inessaPhone = PhoneDirectoryWithPrint("Инесса", 88002500890)
-    inessaPhone.printInfo()
-
-
 
 fun main() {
     val contactsList = mutableListOf<TelephoneDirectoryWithAllRecords>()
@@ -28,8 +22,13 @@ fun main() {
     contactsList += tigranPhone
     val artemyPhone = TelephoneDirectoryWithAllRecords("Артемий", 84959261800, "Студия Лебедева")
     contactsList += artemyPhone
-    val evgeniyPhone = TelephoneDirectoryWithAllRecords("Артемий", 88007008811, null)
+    val evgeniyPhone = TelephoneDirectoryWithAllRecords("Евгений", 88007008811, null)
     contactsList += evgeniyPhone
-    val dmitryPhone = TelephoneDirectoryWithAllRecords("Артемий", 848001006427, "null")
+    val dmitryPhone = TelephoneDirectoryWithAllRecords("Дмитрий", 848001006427, "null")
     contactsList += dmitryPhone
+
+    println("Список всех компаний:")
+    contactsList.forEach {
+        if (it.company != null) println(it.company)
+    }
 }
