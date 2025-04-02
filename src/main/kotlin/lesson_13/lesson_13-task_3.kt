1b67be9dd580f9b0bded812ed97e980e24160dfd
alexeybagrashov <alexey.bagryashov@yandex.ru>
2025-04-02 14:52:10 +0300

    KS-13-3 add fix by comments

--- src/main/kotlin/lesson_13/lesson_13-task_3.kt
+++ src/main/kotlin/lesson_13/lesson_13-task_3.kt
@@ -3,12 +3,21 @@ package org.example.lesson_13
 class TelephoneDirectoryWithAllRecords(
     val name: String,
     val phoneNumber: Long,
-    val company: String?,
+    val company: String? = null
 ) {
-    fun printInfo() {
-        println("$name, $phoneNumber, $company")
-    }
+    fun printInfo() =
+        println(
+            "- Имя: $name\n" +
+                    "- Номер: $phoneNumber\n" +
+                    "- Компания: ${company ?: "не указано"}",
+        )
 }
+
+fun main() {
+    val inessaPhone = PhoneDirectoryWithPrint("Инесса", 88002500890)
+    inessaPhone.printInfo()
+
+
 
 fun main() {
     val contactsList = mutableListOf<TelephoneDirectoryWithAllRecords>()
