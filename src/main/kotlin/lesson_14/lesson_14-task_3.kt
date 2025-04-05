36b88d031fe9dd94ea04fbb3113dd2d5767bac0c
alexeybagrashov <alexey.bagryashov@yandex.ru>
2025-04-05 15:42:32 +0300

    KS-14-3 add figure class

--- src/main/kotlin/lesson_14/lesson_14-task_3.kt
+++ src/main/kotlin/lesson_14/lesson_14-task_3.kt
@@ -3,33 +3,57 @@ package org.example.lesson_14
 import kotlin.math.PI
 
 abstract class Figure(
-    color: String,
+    val color: String,
 ) {
-    open fun calculateAreaOfShape() = println("Площадь фигуры:")
+    open fun calculateArea(): Double = 0.0
 
-    open fun calculatePerimeterOfShape() = println("Периметр:")
+    open fun calculatePerimeter(): Double = 0.0
 }
 
 class Circle(
     color: String,
-    val radius: Int,
+    val radius: Double,
 ) : Figure(color) {
-    override fun calculateAreaOfShape() {
-        super.calculateAreaOfShape()
-        println(PI * radius * radius)
-    }
+    override fun calculateArea(): Double = PI * radius * radius
+
+    override fun calculatePerimeter(): Double = NUMBER_TWO * PI * radius
 }
 
 class Rectangle(
     color: String,
-    val length: Int,
-    val width: Int,
+    val length: Double,
+    val width: Double,
 ) : Figure(color) {
-    override fun calculateAreaOfShape() {
-        super.calculateAreaOfShape()
-        println(length * width)
-    }
+    override fun calculateArea(): Double = length * width
+
+    override fun calculatePerimeter(): Double = NUMBER_TWO * (length + width)
 }
 
 fun main() {
+    val figureList = mutableListOf<Figure>()
+    val whiteCircle = Circle("белый", 10.0)
+
+    val blackCircle = Circle("черный", 4.4)
+    figureList += blackCircle
+
+    val whiteRectangle = Rectangle("белый", 23.7, 4.0)
+    figureList += whiteRectangle
+    val blackRectangle = Rectangle("черный", 2.7, 44.44)
+    figureList += blackRectangle
+
+    var sumOfPerimetersAllBlackPieces = 0.0
+    var sumOfAreasAllWhitePieces = 0.0
+
+    figureList.forEach {
+        if (it.color == "черный") {
+            sumOfPerimetersAllBlackPieces += it.calculatePerimeter()
+        } else {
+            sumOfAreasAllWhitePieces += it.calculateArea()
+        }
+    }
+
+    println("Сумма периметров всех черных фигур: " + sumOfPerimetersAllBlackPieces)
+    println("Сумма площадей всех белых фигур:" + sumOfAreasAllWhitePieces)
 }
+
+const val NUMBER_TWO = 2
