766c1491d1bf3605a830bfac3b510f98c82919dd
alexeybagrashov <alexey.bagryashov@yandex.ru>
2025-04-15 22:03:46 +0300

    KS-16-5 add player class

---
+++ src/main/kotlin/lesson_16/lesson_16-task_5.kt
@@ -0,0 +1,47 @@
+package org.example.Lesson_16
+
+class Player(
+    val nave: String,
+    private var health: Int = HEALTH_MAX,
+    var impactForce: Int = IMPACT_FORCE,
+) {
+    var powerOfTreatment = POWER_OF_TREATMENT
+
+    fun setHealthAfterImpact() {
+        println("Ты пропустил удар! -${impactForce}hp")
+        health -= impactForce
+        if (health <= NUMBER_ZERO) setImpactForceInDeath()
+    }
+
+    fun setHealthAfterTreatment() {
+        if (health > NUMBER_ZERO) {
+            println("Тебя подлечили! +${powerOfTreatment}hp")
+            health += powerOfTreatment
+        }
+    }
+
+    private fun setImpactForceInDeath() {
+        impactForce = NUMBER_ZERO
+        health = NUMBER_ZERO
+        powerOfTreatment = NUMBER_ZERO
+    }
+}
+
+fun main() {
+    val alex = Player("Alex")
+
+    println("Fight!")
+
+    while (alex.powerOfTreatment > 0) {
+        alex.setHealthAfterImpact()
+        alex.setHealthAfterTreatment()
+    }
+
+    println("GAME OVER")
+}
+
+const val HEALTH_MAX = 100
+const val NUMBER_ZERO = 0
+const val IMPACT_FORCE = 20
+const val POWER_OF_TREATMENT = 10
+
