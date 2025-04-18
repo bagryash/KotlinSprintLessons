9c1f8a6bfc830cfa2a2781a5b289cb2b94f2a8d4
alexeybagrashov <alexey.bagryashov@yandex.ru>
2025-04-18 18:02:07 +0300

    KS-16-4 add fix by comments

--- src/main/kotlin/lesson_16/lesson_16-task_4.kt
+++ src/main/kotlin/lesson_16/lesson_16-task_4.kt
@@ -1,23 +1,20 @@
 package org.example.Lesson_16
 
 class Order(
-    val number: Int,
+    private val number: Int,
     private var isOrderReady: Boolean = false,
 ) {
-    fun setStatusReady(status: Boolean) {
+    private fun setStatusReady(status: Boolean) {
         isOrderReady = status
     }
-}
 
-fun createRequestToManager(
-    order: Order,
-    status: Boolean,
-) {
-    order.setStatusReady(status)
+    fun createRequestToManager(status: Boolean) {
+        setStatusReady(status)
+    }
 }
 
 fun main() {
     val order1 = Order(1)
 
-    createRequestToManager(order1, true)
+    order1.createRequestToManager(true)
 }
