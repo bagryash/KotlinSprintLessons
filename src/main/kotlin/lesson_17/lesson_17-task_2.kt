4cf71e3696963a81296b39fdde32ca9089d9bbeb
alexeybagrashov <alexey.bagryashov@yandex.ru>
2025-04-20 17:22:44 +0300

    KS-17-2 add ship class and setter

---
+++ src/main/kotlin/lesson_17/lesson_17-task_2.kt
@@ -0,0 +1,21 @@
+package org.example.lesson_17
+
+class Ship(
+    _name: String,
+    _speedMax: Int,
+    _shipsHomePort: String,
+) {
+    var name = _name
+        set(value) {
+            println("Невозможно переименовать корабль!")
+        }
+    val speedMax = _speedMax
+    val shipsHomePort = _shipsHomePort
+}
+
+fun main() {
+    val aurora = Ship("Аврора", 19, "Санкт-Петербург")
+
+    aurora.name = "Варяг"
+    println(aurora.name)
+}
