907e82185eab8994ecc9dae63f6b8fdce056572a
alexeybagrashov <alexey.bagryashov@yandex.ru>
2025-04-27 21:15:56 +0300

    18-5 add screen class

--- src/main/kotlin/lesson_18/lesson_18-task_5.kt
+++ src/main/kotlin/lesson_18/lesson_18-task_5.kt
@@ -1,21 +1,43 @@
 package org.example.lesson_18
 
-import org.example.lesson_14.Circle
-
-class Screen(
-    val circle: String,
-) {
+class Screen {
+    val circle: String = "круг"
+    val square: String = "квадрат"
+    val point: String = "точка"
 
     fun drawCircle(
         x: Int,
         y: Int,
-    ) = println("Фигура $figure c координатами x: $x, $y")
+    ) = println("Фигура $circle c координатами x: $x, $y")
 
     fun drawCircle(
         x: Float,
         y: Float,
-    ) = println("Фигура $figure c координатами x: $x, $y")
+    ) = println("Фигура $circle c координатами x: $x, у:  $y")
+
+    fun drawSquare(
+        x: Int,
+        y: Int,
+    ) = println("Фигура $square c координатами x: $x, у:  $y")
+
+    fun drawSquare(
+        x: Float,
+        y: Float,
+    ) = println("Фигура $square c координатами x: $x, у: $y")
+
+    fun drawPoint(
+        x: Int,
+        y: Int,
+    ) = println("Фигура $point c координатами x: $x, у:  $y")
+
+    fun drawPoint(
+        x: Float,
+        y: Float,
+    ) = println("Фигура $point c координатами x: $x, у: $y")
 }
 
 fun main() {
+    val screen = Screen()
+    screen.drawPoint(100, 50)
+    screen.drawPoint(100.0f, 50.0f)
 }
