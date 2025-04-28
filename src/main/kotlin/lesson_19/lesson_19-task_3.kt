55e41382b3eaf4b4ff020a4167dd2d199334945e
alexeybagrashov <alexey.bagryashov@yandex.ru>
2025-04-28 20:21:51 +0300

    KS-19-3 add starship class with todo

---
+++ src/main/kotlin/lesson_19/lesson_19-task_3.kt
@@ -0,0 +1,22 @@
+package org.example.lesson_19
+
+class Starship {
+    fun startTakeOff() {
+        // TODO дописать логику
+    }
+
+    fun startLanding() {
+        //  TODO Дописать логику
+    }
+
+    fun startShooting() {
+        TODO("Дописать по требованиям")
+    }
+}
+
+fun main() {
+    val starship = Starship()
+    starship.startTakeOff()
+    starship.startLanding()
+    starship.startShooting()
+}
