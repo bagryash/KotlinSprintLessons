0f38ae39257980f5b826316c9f3ecc3863503d0f
alexeybagrashov <alexey.bagryashov@yandex.ru>
2025-04-29 18:29:39 +0300

    KS-19-5 add file cabinet application

---
+++ src/main/kotlin/lesson_19/lesson_19-task_5.kt
@@ -0,0 +1,49 @@
+package org.example.lesson_19
+
+enum class Gender(
+    gender: String,
+) {
+    MAN("M"),
+    WOMAN("W"),
+}
+
+fun getGender(id: String): Gender =
+    if (id == "M") {
+        Gender.MAN
+    } else {
+        Gender.WOMAN
+    }
+
+class Person(
+    val name: String,
+    val gender: Gender,
+)
+
+fun main() {
+    var fileCabinet = listOf<Person>()
+    var counter = FIVE
+
+    println(
+        """
+            Добро пожаловать в \"Картотеку\"!
+        Здесь можно создавать и хранить данные имя человека и его пол. 
+        Данные вводятся:
+        Имя - кириллицей или латиницей
+        Пол - M(мужской) или W(женский) только латиницей
+        
+        """.trimIndent(),
+    )
+
+    while (counter != 0) {
+        println("Введите по очереди данные человека:")
+        val person = Person(name = readln(), gender = getGender(readln()))
+        fileCabinet += person
+        counter--
+    }
+
+    fileCabinet.forEach {
+        println(it.name + ", " + it.gender)
+    }
+}
+
+const val FIVE = 5
