88632dd5683b77a6d4fc701c063bd02d350a60f8
alexeybagrashov <alexey.bagryashov@yandex.ru>
2025-05-05 22:45:57 +0300

    KS-21-2 add fix

--- src/main/kotlin/lesson_21/lesson_21-task_5.kt
+++ src/main/kotlin/lesson_21/lesson_21-task_5.kt
@@ -1,28 +1,27 @@
 package org.example.lesson_21
 
 fun Map<String, Int>.maxCategory(): String {
-    var v = 0
-    var k: String? = null
-
-   forEach {
-        if (it.value > v) {
-            v = it.value
-            k = it.key
+    var skillMax = 0
+    var numberOfExperience: String? = null
+    forEach {
+        if (it.value > skillMax) {
+            skillMax = it.value
+            numberOfExperience = it.key
         }
     }
-    return k!!
+    return numberOfExperience!!
 }
 
 fun main() {
     val experience =
         mapOf(
             "скорость" to 79,
-            "удары" to 81,
+            "удары" to 84,
             "передачи" to 77,
             "дриблинг" to 75,
             "защита" to 79,
             "физ подготовка" to 84,
         )
 
-    experience.maxCategory()
+    println(experience.maxCategory())
 }
