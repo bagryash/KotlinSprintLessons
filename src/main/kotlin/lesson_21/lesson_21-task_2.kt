3ab327b42df3fe86ea3ad8f152f6bbbe6aac87b7
alexeybagrashov <alexey.bagryashov@yandex.ru>
2025-05-05 22:36:11 +0300

    KS-21-2 add fix

--- src/main/kotlin/lesson_21/lesson_21-task_2.kt
+++ src/main/kotlin/lesson_21/lesson_21-task_2.kt
@@ -1,15 +1,15 @@
 package org.example.lesson_21
 
 fun main() {
-    fun List<Int>.evenNumbersSum(list: List<Int>): Int {
+    fun List<Int>.evenNumbersSum(): Int {
         var sumOfEvenNumbers = 0
-        list.forEach {
+       forEach {
             if (it % 2 == 0) sumOfEvenNumbers += it
         }
         return sumOfEvenNumbers
     }
 
     val listOfInt = listOf(1, 32, 5, 56, 61, 89, 80)
-    println(listOfInt.evenNumbersSum(listOfInt))
+    println(listOfInt.evenNumbersSum())
 
 }
