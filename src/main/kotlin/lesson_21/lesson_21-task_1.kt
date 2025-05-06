e6ae31e86c96a9d46e115f96004e255d0b10b884
alexeybagrashov <alexey.bagryashov@yandex.ru>
2025-05-06 17:25:01 +0300

    KS-21-1 add fix by comment

--- src/main/kotlin/lesson_21/lesson_21-task_1.kt
+++ src/main/kotlin/lesson_21/lesson_21-task_1.kt
@@ -2,7 +2,7 @@ package org.example.lesson_21
 
 fun String.vowelCount(): Int {
     val vowels = "aeiouAEIOU"
-    return  this.count { it in vowels }
+    return this.count { it in vowels }
 }
 
 fun main() {
